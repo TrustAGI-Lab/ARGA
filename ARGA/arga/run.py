1a3373e58ced2491d034ca1339fb4a4870516caf
Ruiqi Hu <ruiqi.hu.1992@gmail.com>
2018-07-25 23:10:12 +1000

    Update run.py

--- ARGA/arga/run.py
+++ ARGA/arga/run.py
@@ -4,9 +4,9 @@ from clustering import Clustering_Runner
 from link_prediction import Link_pred_Runner
 
 
-dataname = 'citeseer'       # 'cora' or 'citeseer' or 'pubmed'
-model = 'arga_vae'          # 'arga_ae' or 'arga_vae'
-task = 'clustering'         # 'clustering' or 'link_prediction'
+dataname = 'cora'       # 'cora' or 'citeseer' or 'pubmed'
+model = 'arga_ae'          # 'arga_ae' or 'arga_vae'
+task = 'link_prediction'         # 'clustering' or 'link_prediction'
 
 settings = settings.get_settings(dataname, model, task)
 
